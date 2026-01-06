1035fe858d0b1037cd3742829de19935d9144d51
JotaVictor12 <131072638+JotaVictor12@users.noreply.github.com>
2026-01-06 16:45:01 -0300

    Create main.js

---
+++ Valida CPF/main.js
@@ -0,0 +1,44 @@
+function Validacpf(cpf) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function() {
+            return cpf.replace(/\D+/g, '');
+        }
+    });
+}
+
+Validacpf.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigitos(cpfParcial);
+    const digito2 = this.criaDigitos(cpfParcial + digito1);
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+};
+
+Validacpf.prototype.criaDigitos = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo --;
+        return ac;
+    }, 0)
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+};
+
+Validacpf.prototype.isSequencia = function(){
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+
+const cpf = new Validacpf('476.916.688.56');
+if(cpf.valida()) {
+    console.log('CPF válido');
+} else {
+    console.log('CPF inválido');
+}
